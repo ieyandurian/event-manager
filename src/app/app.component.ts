ea93464497279c5d5d89c5508d4e408cb4be56bf
ieyandurian <jijol1994@gmail.com>
2017-09-21 10:34:18 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,42 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { Component } from "@angular/core";
+import { Platform } from "ionic-angular";
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
 
-import { HomePage } from '../pages/home/home';
+import { HomePage } from "../pages/home/home";
+
+import firebase from "firebase";
+
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: "app.html"
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen
+  ) {
+    firebase.initializeApp({
+      apiKey: "AIzaSyC1A-c5WVw9-7A1V7MF0yM82-KOhOhYUB8",
+      authDomain: "eventmanager-9799e.firebaseapp.com",
+      databaseURL: "https://eventmanager-9799e.firebaseio.com",
+      projectId: "eventmanager-9799e",
+      storageBucket: "",
+      messagingSenderId: "936720721712"
+    });
+
+    const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+      if (!user) {
+        this.rootPage = "LoginPage";
+        unsubscribe();
+      } else {
+        this.rootPage = HomePage;
+        unsubscribe();
+      }
+    });
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -19,4 +45,3 @@ export class MyApp {
     });
   }
 }
-
